a9e8c9a380fbc439294d6d06737d7f298e8c467e
termgiles <Thomas.giles2222@gmail.com>
2024-11-19 15:51:50 +0000

    Add LikesAndDislikes Reaction method, plus tests, task 4

---
+++ cs-TDD-and-Communication-Test/LikesAndDislikesTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using cs_TDD_and_Communication;
+using FluentAssertions;
+
+namespace cs_TDD_and_Communication_Test
+{
+    public class LikesAndDislikesTests
+    {
+        //single input tests
+        [Test]
+        [TestCase(new Reactions[] {Reactions.LIKE}, Reactions.LIKE)]
+        [TestCase(new Reactions[] { Reactions.DISLIKE }, Reactions.DISLIKE)]
+        [TestCase(new Reactions[] { Reactions.NEUTRAL }, Reactions.NEUTRAL)]        //n.b. shouldn't be able to input this
+        [TestCase(new Reactions[] {}, Reactions.NEUTRAL)]
+
+        public void TestReaction(Reactions[] inputReactions, Reactions expectedResult )
+        {
+            expectedResult.Should().Be( LikesAndDislikes.CalculateReaction(inputReactions) );
+        }
+
+        //multiple input tests
+        [Test]
+        [TestCase(new Reactions[] { Reactions.LIKE, Reactions.LIKE }, Reactions.NEUTRAL)]
+        [TestCase(new Reactions[] { Reactions.DISLIKE, Reactions.DISLIKE }, Reactions.NEUTRAL)]
+        [TestCase(new Reactions[] { Reactions.LIKE, Reactions.DISLIKE }, Reactions.NEUTRAL)]
+        [TestCase(new Reactions[] { Reactions.LIKE, Reactions.LIKE, Reactions.LIKE }, Reactions.LIKE, TestName = "three likes")]
+        [TestCase(new Reactions[] { Reactions.DISLIKE, Reactions.DISLIKE, Reactions.DISLIKE }, Reactions.DISLIKE)]
+        [TestCase(new Reactions[] { Reactions.LIKE, Reactions.LIKE, Reactions.DISLIKE }, Reactions.DISLIKE)]
+        [TestCase(new Reactions[] { Reactions.LIKE, Reactions.DISLIKE, Reactions.DISLIKE }, Reactions.LIKE)]
+
+        public void TestMultipleReactions(Reactions[] inputReactions, Reactions expectedResult)
+        {
+            expectedResult.Should().Be(LikesAndDislikes.CalculateReaction(inputReactions));
+        }
+
+
+        //edge cases
+    }
+}
---
+++ cs-TDD-and-Communication/LikesAndDislikes.cs
@@ -0,0 +1,29 @@
+﻿namespace cs_TDD_and_Communication
+{
+    
+    public enum Reactions
+    {
+        LIKE,
+        DISLIKE,
+        NEUTRAL
+    }
+    
+    public class LikesAndDislikes 
+    {
+        public static Reactions CalculateReaction(Reactions[] userReactions)
+        {
+            int likes = 0, dislikes = 0;
+            foreach (Reactions reaction in userReactions)
+            {
+                if (reaction == Reactions.LIKE) likes++;
+                if (reaction == Reactions.DISLIKE) dislikes++;
+            }
+            bool trueLike = likes % 2 != 0;
+            bool trueDislike = dislikes % 2 != 0;
+            if(trueLike && trueDislike) return Reactions.NEUTRAL;
+            if(trueLike) return Reactions.LIKE;
+            if (trueDislike) return Reactions.DISLIKE;
+            return Reactions.NEUTRAL;
+        }   
+    }
+}
