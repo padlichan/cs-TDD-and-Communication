db4979a1da55e589b4fe30de652528c65ba99f77
Lili <lili.berenyi@gmail.com>
2024-11-19 10:27:34 +0000

    Add empty projects

---
+++ cs-TDD-and-Communication-Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace cs_TDD_and_Communication_Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
